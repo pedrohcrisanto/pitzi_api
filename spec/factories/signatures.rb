7205a3b33d4caf882ff31f7e217998047a6487df
pedrohcrisanto@gmail.com <pedrohcrisanto@gmail.com>
2021-01-25 15:27:22 -0300

    modify factory

--- spec/factories/signatures.rb
+++ spec/factories/signatures.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     cellphone_model { Faker::PhoneNumber.cell_phone_in_e164 }
     imei { Faker::Code.imei  }
     price { Faker::Number.decimal(l_digits: 3, r_digits: 3) }
-    number_portion { 2..12 }
+    number_portion { Faker::Random.rand(1..12) }
     client { Client.all.sample }
   end
 end
