78f37ba38eb882761e7473bae1137536587a450b
pedrohcrisanto@gmail.com <pedrohcrisanto@gmail.com>
2021-01-25 12:10:13 -0300

    validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,5 @@
 class Client < ApplicationRecord
   has_many :signatures
+
+  validates :cpf, presence: true, uniqueness: true
 end
--- app/models/signature.rb
+++ app/models/signature.rb
@@ -1,3 +1,5 @@
 class Signature < ApplicationRecord
   belongs_to :client
+
+  validates :price, :imei, :cellphone_model, presence: true
 end
